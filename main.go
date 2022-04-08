e3cb7675f78a3cf6ba5e3eef2dd3f7cd2ba06b8a
Arhimedeς Montegasppα <cacilhas@valfenda.local>
2022-04-08 20:55:38 -0300

    let’s start again…

--- main.go
+++ main.go
@@ -1,30 +1,5 @@
 package main
 
-import (
-	"math/rand"
-	"time"
-
-	"github.com/cacilhas/sudogo/ui"
-	raylib "github.com/gen2brain/raylib-go/raylib"
-	"github.com/spf13/viper"
-)
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
-}
-
 func main() {
-	readSettings()
-	defer saveSettings()
-
-	raylib.InitWindow(
-		viper.GetInt32("width"),
-		viper.GetInt32("height"),
-		"Sudogo",
-	)
-	defer raylib.CloseWindow()
-	raylib.SetWindowMinSize(600, 600)
-	raylib.SetWindowState(raylib.FlagWindowResizable)
-	raylib.SetTargetFPS(24)
-	ui.Mainloop()
+	println("Hello, world.")
 }
