9911e5124821adf7b0f21e21afc2757d3db59df5
Arhimedeς Montegasppα <cacilhas@valfenda.local>
2022-04-10 00:04:21 -0300

    draw game

--- ui/gameplay.go
+++ ui/gameplay.go
@@ -1,6 +1,8 @@
 package ui
 
 import (
+	"image/color"
+
 	"github.com/cacilhas/sudogo/sudoku"
 	raylib "github.com/gen2brain/raylib-go/raylib"
 	"github.com/spf13/viper"
@@ -8,6 +10,19 @@ import (
 
 type gameplayType struct {
 	sudoku.Game
+}
+
+var colours [10]color.RGBA = [10]color.RGBA{
+	raylib.Black,
+	raylib.Red,
+	raylib.Orange,
+	raylib.Yellow,
+	raylib.Green,
+	raylib.SkyBlue,
+	raylib.Blue,
+	raylib.Violet,
+	raylib.Pink,
+	raylib.DarkGray,
 }
 
 func startGameplay(level sudoku.Level) Scene {
@@ -30,10 +45,10 @@ func (gameplay *gameplayType) Render() Scene {
 	if width < boardSize {
 		boardSize = width
 	}
-	//game := gameplay.Game
 	xOffset := (width - boardSize) / 2
 	yOffset := (height - boardSize) / 2
 	drawBoard(xOffset, yOffset, boardSize)
+	drawGame(xOffset, yOffset, boardSize/9, gameplay.Game)
 
 	return gameplay
 }
@@ -58,5 +73,37 @@ func drawBoard(x, y, size int32) {
 	}
 	for ly := y; ly <= y+cell*10; ly += cell {
 		raylib.DrawLine(x, ly, x+cell*9, ly, raylib.Black)
+	}
+}
+
+func drawGame(sx, sy, cellSize int32, game sudoku.Game) {
+	smallSize := cellSize / 3
+	for y := 0; y < 9; y++ {
+		for x := 0; x < 9; x++ {
+			ix := sx + int32(x)*cellSize
+			iy := sy + int32(y)*cellSize
+			cell := game.Get(x, y)
+			if cell.IsSet() {
+				raylib.DrawCircle(
+					ix+cellSize/2,
+					iy+cellSize/2,
+					float32(cellSize)/2,
+					colours[cell.Value()],
+				)
+			} else {
+				for i := 1; i <= 9; i++ {
+					if cell.Candidate(i) {
+						ix2 := int32(i-1) % 3
+						iy2 := int32(i-1) / 3
+						raylib.DrawCircle(
+							ix+int32(ix2)*smallSize+smallSize/2,
+							iy+int32(iy2)*smallSize+smallSize/2,
+							float32(smallSize)/2,
+							colours[i],
+						)
+					}
+				}
+			}
+		}
 	}
 }
